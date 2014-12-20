e6109920f10f9d5df99351f2254ad5647c425dbf
Mateusz <mateusz.przybylski@koditto.pl>
2014-12-20 08:14:04 +0100

    key disabler import from e975 device tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,3 +168,5 @@ TW_MAX_BRIGHTNESS := 255
 
 #Asserts
 TARGET_OTA_ASSERT_DEVICE := gee,geeb,geehrc,e975,geehrc_4g_spr,geespr,ls970,geehrc4g,geehrc4g_spr_us,geebus,e970,e973,e971,geeb_att_us
+
+BOARD_HARDWARE_CLASS += device/lge/e975/cmhw
