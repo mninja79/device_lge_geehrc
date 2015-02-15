8b196407760fc2840f07ab69809b1fb1db45761d
dadi11 <dadi.sikorski@gmail.com>
2015-02-15 14:18:58 +0100

    commit

--- aicp.mk
+++ aicp.mk
@@ -1,18 +1,26 @@
-#
-# Copyright 2013 The Android Open-Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 768
+
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+$(call inherit-product, vendor/aicp/configs/bootanimation.mk)
+$(call inherit-product, vendor/aicp/configs/gsm.mk)
+
+# Inherit device configuration
 $(call inherit-product, device/lge/geehrc/full_geehrc.mk)
 
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := geehrc
 PRODUCT_NAME := aicp_geehrc
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := Optimus G
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.4.2 KOT49H 937116 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
